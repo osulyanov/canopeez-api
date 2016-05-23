ca1ee9cc228ed45c50cc1c307b28d0167ceafd3f
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-23 16:25:11 +0700

    Add tests for Users#sing_in - negative cases

--- spec/requests/users_controller.rb
+++ spec/requests/users_controller.rb
@@ -4,17 +4,34 @@ describe 'Api::V1::UsersController', type: :request do
   describe 'POST #sign_in' do
     let!(:user) { create :user, email: 'user@example.com', password: 'pass123' }
 
-    before do
-      post '/api/v1/users/sign_in', email: 'user@example.com',
-           password: 'pass123'
+    context 'correct data' do
+      before do
+        post '/api/v1/users/sign_in', email: 'user@example.com',
+                                      password: 'pass123'
+      end
+
+      it { expect(response).to have_http_status(:success) }
+
+      it 'returns current user as json' do
+        result = json['data']['id']
+
+        expect(result).to eq(user.id)
+      end
     end
 
-    it { expect(response).to have_http_status(:success) }
+    context 'incorrect data' do
+      before do
+        post '/api/v1/users/sign_in', email: 'user@example.com',
+                                      password: 'wrongpass'
+      end
 
-    it 'returns current user as json' do
-      result = json['data']['id']
+      it { expect(response).to have_http_status(:unauthorized) }
 
-      expect(result).to eq(user.id)
+      it 'returns error' do
+        result = json['errors']
+
+        expect(result).to be_present
+      end
     end
   end
 end
