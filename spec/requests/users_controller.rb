fc98876e087c4df10408717d785513f216044f36
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-23 16:55:37 +0700

    Add tests for Users Sign Up

--- spec/requests/users_controller.rb
+++ spec/requests/users_controller.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Api::V1::UsersController', type: :request do
-  describe 'POST #sign_in' do
+  describe 'Sign In' do
     let!(:user) { create :user, email: 'user@example.com', password: 'pass123' }
 
     context 'correct data' do
@@ -31,6 +31,50 @@ describe 'Api::V1::UsersController', type: :request do
         result = json['errors']
 
         expect(result).to be_present
+      end
+    end
+  end
+
+  describe 'Sign Up' do
+    context 'correct data' do
+      before do
+        post '/api/v1/users.json', email: 'em@il.ru', password: '123456',
+                                   password_confirmation: '123456'
+      end
+
+      it { expect(response).to have_http_status(:success) }
+
+      it 'returns success status as json' do
+        result = json['status']
+
+        expect(result).to eq('success')
+      end
+
+      it 'returns current user as json' do
+        result = json['data']['id']
+
+        expect(result).to be_present
+      end
+    end
+
+    context 'incorrect data' do
+      before do
+        post '/api/v1/users.json', email: 'em@il.ru', password: '123456',
+                                   password_confirmation: '12345'
+      end
+
+      it { expect(response).to have_http_status(:forbidden) }
+
+      it 'returns error status as json' do
+        result = json['status']
+
+        expect(result).to eq('error')
+      end
+
+      it 'returns current user as json' do
+        result = json['data']['id']
+
+        expect(result).to be_blank
       end
     end
   end
