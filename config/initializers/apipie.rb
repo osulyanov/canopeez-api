cc3eefb871ade4dd41baa5d235a28f007181d3ec
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-23 20:31:34 +0700

    Configure api-pie

---
+++ config/initializers/apipie.rb
@@ -0,0 +1,16 @@
+Apipie.configure do |config|
+  config.app_name = 'Canopeez'
+  config.api_base_url = '/api'
+  config.doc_base_url = '/apidoc'
+  config.reload_controllers = Rails.env.development?
+  config.api_controllers_matcher = File.join(Rails.root, 'app', 'controllers',
+                                             'api', '**', '**')
+  config.api_routes = Rails.application.routes
+  config.default_version = 'V1'
+  config.validate = true
+  config.app_info['V1'] = <<-EOS
+  EOS
+  config.default_locale = 'en'
+  config.markup = Apipie::Markup::Markdown.new
+  config.validate = false
+end
