25d4492aacc05c9626115fff8ecc53cb3d1b3b5f
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-24 22:13:32 +0700

    Add Categories model

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+class Category < ActiveRecord::Base
+  validates :name, presence: true
+end
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
