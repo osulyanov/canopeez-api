69d607121cba1ee845b8156025d509ed17c07cf8
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-25 14:14:26 +0700

    Add Founder admin resource

---
+++ app/admin/founder.rb
@@ -0,0 +1,34 @@
+ActiveAdmin.register Founder do
+  permit_params :startup_id, :name, :surname, :position, :description, :quote,
+                :linkedin_url, :photo_url
+
+  index do
+    selectable_column
+    id_column
+    column :startup
+    column :user
+    column :full_name
+    column :position
+    actions
+  end
+
+  filter :startup
+  filter :user
+  filter :name
+  filter :surname
+  filter :position
+
+  form do |f|
+    f.inputs 'Founder Details' do
+      f.input :startup
+      f.input :name
+      f.input :surname
+      f.input :position
+      f.input :description
+      f.input :quote
+      f.input :linkedin_url
+      f.input :photo_url
+    end
+    f.actions
+  end
+end
