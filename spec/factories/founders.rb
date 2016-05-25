98963a2272eb9fb771a0f61348f907bc195d6335
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-25 13:26:30 +0700

    Add Founder factory

---
+++ spec/factories/founders.rb
@@ -0,0 +1,29 @@
+FactoryGirl.define do
+  factory :founder do
+    startup
+    name 'Nikolay'
+    surname 'Petrov'
+    position 'SEO'
+  end
+end
+
+# == Schema Information
+#
+# Table name: founders
+#
+#  id           :integer          not null, primary key
+#  startup_id   :integer
+#  name         :string
+#  surname      :string
+#  position     :string
+#  description  :text
+#  quote        :text
+#  linkedin_url :string
+#  photo_url    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_founders_on_startup_id  (startup_id)
+#
