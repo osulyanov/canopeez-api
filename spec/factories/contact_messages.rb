63f4f24862e00b7906d7a92ac144e2ad44aba23a
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-25 15:44:50 +0700

    Add ContactMessage factory

---
+++ spec/factories/contact_messages.rb
@@ -0,0 +1,25 @@
+FactoryGirl.define do
+  factory :contact_message do
+    message 'Hey folks'
+  end
+end
+
+# == Schema Information
+#
+# Table name: contact_messages
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  message    :text
+#  action     :integer
+#  mail       :string
+#  city       :string
+#  full_name  :string
+#  type       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_contact_messages_on_user_id  (user_id)
+#
