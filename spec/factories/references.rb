280bb13be132c6fad81e42b10e64971b47abfeab
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-25 14:41:27 +0700

    Add Reference factory

---
+++ spec/factories/references.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+  factory :reference do
+    startup
+    url 'http://yandex.ru'
+    description 'Yandex'
+  end
+end
+
+# == Schema Information
+#
+# Table name: references
+#
+#  id          :integer          not null, primary key
+#  startup_id  :integer
+#  url         :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_references_on_startup_id  (startup_id)
+#
