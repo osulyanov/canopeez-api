640d1d2bb0116a84bff43921c182861f5aee9886
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-25 21:34:08 +0700

    Add routes for Startups contoller

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,10 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
-    scope :v1 do
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users'
+      resources :startups
     end
   end
 end
