a1d038fa177fd0d14c3081eee9024180d9f85887
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-26 23:01:34 +0700

    Add routes for Categories API

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users'
       resources :startups
+      resources :categories, only: [:index]
     end
   end
 end
