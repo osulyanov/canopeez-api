7df165982861df9bdae66d7c1f6285df35037103
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-30 16:01:52 +0700

    Add Partner#index API

---
+++ app/controllers/api/v1/partners_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class PartnersController < Api::V1::V1Controller
+      before_action :authenticate_user!
+      load_and_authorize_resource
+
+      resource_description do
+        short 'Partners'
+      end
+
+      api :GET, '/v1/partners', 'Partners list'
+      description <<-EOS
+        ## Description
+        List of all partners sorted by name.
+      EOS
+      example <<-EOS
+        [
+          {
+            "id": 1,
+            "name": "Yandex",
+            "logo_url": "1"
+          }
+        ]
+      EOS
+
+      def index
+        @partners = Partner.alphabet
+        respond_with :api, :v1, @partners
+      end
+    end
+  end
+end
