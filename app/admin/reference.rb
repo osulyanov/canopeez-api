abaefb8104a088069d1fe3c070cd9975dc00f146
Oleg Sulyanov <oleg@sulyanov.com>
2016-05-31 07:22:16 +0700

    Add  Reference admin resource

--- app/admin/reference.rb
+++ app/admin/reference.rb
@@ -1,12 +1,15 @@
 ActiveAdmin.register Reference do
-  permit_params :name, :description
+  permit_params :startup_id, :url, :description
 
   belongs_to :startup, optional: true
 
   index do
     selectable_column
     id_column
-    column :name
+    column :user
+    column :startup
+    column :url
+    column(:description) { |e| truncate e.description, length: 100 }
     actions
   end
 
@@ -14,7 +17,8 @@ ActiveAdmin.register Reference do
 
   form do |f|
     f.inputs 'Reference Details' do
-      f.input :name
+      f.input :startup
+      f.input :url
       f.input :description
     end
     f.actions
