78f32f0582c9cb47777c142bdb41d73d037c1149
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-27 22:33:47 +0700

    Set headers

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -33,10 +33,14 @@ module Api
       # Because we want our front-end to send cookies to allow the API to be
       # authenticated (using 'withCredentials' in the XMLHttpRequest), we need
       # to add some headers so the browser will not reject the response
-      response.headers['Access-Control-Allow-Origin'] = '*'
-      response.headers['Access-Control-Allow-Credentials'] = 'true'
+      response.headers['Access-Control-Allow-Origin'] =
+          request.headers['Origin'] || '*'
       response.headers['Access-Control-Allow-Methods'] =
-        'POST, GET, OPTIONS, PUT, DELETE'
+          'POST, PUT, DELETE, GET, OPTIONS'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] =
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      response.headers['Access-Control-Allow-Credentials'] = 'true'
     end
   end
 end
