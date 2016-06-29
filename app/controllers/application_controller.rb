4641ce3fe874dacd86aa0a762303fe64816dacbb
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-29 20:34:16 +0700

    Temporary disable forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session,
-                       if: proc { |c| c.request.format.json? }
+  skip_before_action :verify_authenticity_token
+  # protect_from_forgery with: :null_session,
+  #                      if: proc { |c| c.request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
