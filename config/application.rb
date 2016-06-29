d21b1a0faea2ae922c3507b0e7a942d4441379db
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-29 09:06:50 +0700

    Add CORS to Warden::Manager

--- config/application.rb
+++ config/application.rb
@@ -40,13 +40,16 @@ module Canopeez
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any,
+                 methods: [:get, :post, :options,  :put, :create, :delete]
       end
     end
+
     config.middleware.insert_before Warden::Manager, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any,
+                 methods: [:get, :post, :options, :put, :create, :delete]
       end
     end
   end
