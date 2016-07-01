b83bbf6d79c9a56db9bd572454ee8fcd374a5e45
Oleg Sulyanov <oleg@sulyanov.com>
2016-07-01 17:13:48 +0700

    Annotate Rating factory

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -6,16 +6,18 @@ end
 
 # == Schema Information
 #
-# Table name: favorites
+# Table name: ratings
 #
 #  id         :integer          not null, primary key
 #  user_id    :integer
 #  startup_id :integer
+#  value      :integer          default(0), not null
+#  visible    :boolean          default(TRUE), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 # Indexes
 #
-#  index_favorites_on_startup_id  (startup_id)
-#  index_favorites_on_user_id     (user_id)
+#  index_ratings_on_startup_id  (startup_id)
+#  index_ratings_on_user_id     (user_id)
 #
